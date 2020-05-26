68a3129c20fe5fc8559272a237bfa0872e7c9b7f
Jian Wen <yourgeniusson@gmail.com>
2020-05-26 11:14:08 +0800

    First commitment to this stupid language

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+main() {
+  gay();
+}
+
+class BunchOfFunctions() {
+  var yourMother = "looking kinda thicc";
+  var yourFather = "looking kinda lesbian";
+  gay() {
+    print("Learning flutter is such a pain in the asshole");
+  }
+}
